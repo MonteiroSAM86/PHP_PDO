b399f5cf1959ab1d2e1d741ee7882bed6c22993f
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-15 16:11:27 +0100

    151611jul20

--- index2.php
+++ index2.php
@@ -1,3 +1,5 @@
+
+</HEAD>
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <iframe id="myFrame" src="https://kuula.co/share/collection/7l80D?fs=1&vr=1&zoom=1&autorotate=0.08&thumbs=3&hideinst=1&chromeless=0&logo=1" width="100%" height=100% style="border:none;"></iframe>
